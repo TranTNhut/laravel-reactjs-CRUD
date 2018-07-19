65e8d62f215091ab44b4b9f75dbb9db95cce82a2
tranthanhnhut <34058216+tranthanhnhut@users.noreply.github.com>
2018-07-19 18:08:01 +0700

    add node js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,22 +1,33 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
  */
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
 /**
- * Next, we will create a fresh Vue application instance and attach it to
+ * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//require('./components/Example');
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, Switch } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
 
-const app = new Vue({
-    el: '#app'
-});
+import Home from './components/Example'
+
+const history = createHistory()
+
+render(
+    <Router history={history}>
+        <Switch>
+            <Route path='/' component={Home}></Route>
+        </Switch>
+    </Router>, document.getElementById('app')
+)
+
